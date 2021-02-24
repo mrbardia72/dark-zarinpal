42cbb1a5dd54220ff00e1ba8862e1c099048ffa1
mrbardia72 <bardia.backend@gmail.com>
2021-02-24 12:29:00 +0330

    🌱dark-zarinpal💙- Wednesday-Feb-24-2021-12-29-00

--- config/db.go
+++ config/db.go
@@ -1,1 +1,34 @@
 package config
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func CheckErr(err error)  {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func DbConfig() *mongo.Client {
+
+	ctx := context.Background()
+
+	url := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	connect, err := mongo.Connect(ctx, url)
+	CheckErr(err)
+
+	// Check the connection
+	err = connect.Ping(ctx, nil)
+	CheckErr(err)
+
+	fmt.Println("Connected to MongoDB!")
+	return connect
+}
+
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/sinabakh/go-zarinpal-checkout v0.0.0-20171230121056-f6518b3fddc3
+	go.mongodb.org/mongo-driver v1.4.6
 )
